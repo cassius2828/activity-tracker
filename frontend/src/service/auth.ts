ef99fd540a8ddd82b438f7d156e13a589d90c72d
Cassius Reynolds <cassius.reynolds.dev@gmail.com>
2026-04-22 00:30:52 -0700

    fix: resolve issues in task filtering functionality and improve error handling in profile and tasks pages

---
+++ frontend/src/service/auth.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+type LoginBody = Omit<SignupBody, "confirmPassword">;
+
+type LoginResponse = {
+  message: string;
+  token: string;
+};
+
+type SignupBody = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+const login = async (loginBody: LoginBody) => {
+  try {
+    const response = await axios.post<LoginResponse>(
+      import.meta.env.BACKEND_URL + "/auth/login",
+      loginBody,
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const signup = async (signupBody: SignupBody) => {
+  try {
+    const response = await axios.post<LoginResponse>(
+      import.meta.env.BACKEND_URL + "/auth/signup",
+      signupBody,
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export { login, signup };
